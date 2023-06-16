6625a645e9789b420161e3a699e3f16ce2bc46cd
lina-felina <lina-felina@mail.ru>
2023-06-16 15:15:40 +0500

    задание 1

--- seminar2/Program.cs
+++ seminar2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Семинар 2. Задача 1.
+// ПРОВЕРЯЕМ КАКОЕ ИЗ ДВУХ ЧИСЕЛ БОЛЬШЕ
+
+Console.WriteLine("Введите целое число");
+
+string inputedString1 = Console.ReadLine();
+string inputedString2 = Console.ReadLine();
+
+int number1 = Convert.ToInt32(inputedString1);
+// Console.WriteLine($"Вы ввели число {number1}!");
+int number2 = Convert.ToInt32(inputedString2);
+// Console.WriteLine($"Вы ввели число {number2}!");
+
+if (number1 > number2)
+{ 
+    Console.WriteLine($"Первое чисто {number1} больше второго {number2}"); 
+}
+else 
+ Console.WriteLine($"второе число {number2} больше первого  {number1}");
