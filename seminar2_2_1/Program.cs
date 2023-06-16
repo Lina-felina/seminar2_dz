53f703e88e874ae3486344aa039201eb9b8cfd0a
lina-felina <lina-felina@mail.ru>
2023-06-16 22:39:18 +0500

    задание 2

---
+++ seminar2_2_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Семинар 1. Задача 2.
+Console.WriteLine("самое большое число");
+int a = 44;
+int b = 5;
+int c = 78;
+int max = a;
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+Console.Write("max = ");
+Console.WriteLine(max);
